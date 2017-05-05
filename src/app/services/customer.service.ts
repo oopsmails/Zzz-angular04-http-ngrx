c99f4a7e0dbc7e0e338022c7fa677077f0145cb0
oopsmails <oopsmails@gmail.com>
2017-05-04 22:17:05 -0400

    better reading

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Customer } from '../models/customer.model';
-// import { Observable } from 'rxjs/Rx';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
-import { Store } from '@ngrx/store';
+import { Store, Action } from '@ngrx/store';
 import { GET_CUSTOMERS, RESET } from '../ngstore/customer.actions';
 import { CustomerAppStore } from '../ngstore/customer.appstore';
 
@@ -20,14 +19,20 @@ export class CustomerService {
   }
 
   loadCustomers() {
-    return this.http.get(this.customersUrl)
-      // ...and calling .json() on the response to return data
+    const httpResult: Observable<Action> = this.http.get(this.customersUrl)
       .map((res: Response) => res.json())
       .map(payload => ({ type: 'ADD_CUSTOMERS', payload: { customers: payload, selectedCustomer: new Customer() } }))
-      .catch((error: any) => Observable.throw(error.json().error || 'Server error'))
-      .subscribe((action) => {
-        this.store.dispatch(action);
-      });
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+
+    return httpResult.subscribe(action => this.store.dispatch(action));
+
+    // return this.http.get(this.customersUrl)
+    //   .map((res: Response) => res.json())
+    //   .map(payload => ({ type: 'ADD_CUSTOMERS', payload: { customers: payload, selectedCustomer: new Customer() } }))
+    //   .catch((error: any) => Observable.throw(error.json().error || 'Server error'))
+    //   .subscribe((action) => {
+    //     this.store.dispatch(action);
+    //   });
   }
 
 }
